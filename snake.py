17a827f97b72da79f2a4fab8b5be9705349ab979
@karzelguizio <kawrantin.arzel--guiziou@etudiant.univ-rennes1.fr>
2024-07-24 00:07:58 +0200

    everybody's here but nobody's moving

--- snake.py
+++ snake.py
@@ -5,24 +5,38 @@ import random as rdm
 CELL_SIZE=40
 ROWS=10
 COLUMNS=10
-SNAKE_COLOR='#00FF00'
-APPLE_COLOR='#FF0000'
 BACKGROUND_COLOR_1='#111111'
 BACKGROUND_COLOR_2='#303030'
 
 class Snake:
     def __init__(self):
         self.body_parts = 3
-        self.position = [(1,1), (1,2), (1,3)]
+        self.coordinates = [(1,8), (2,8), (3,8)]
+        self.color = '#00FF00'
+        for (x, y) in self.coordinates:
+            canva.create_rectangle(x*CELL_SIZE, y*CELL_SIZE, x*CELL_SIZE+CELL_SIZE, y*CELL_SIZE+CELL_SIZE, fill=self.color)
+    
+    def add_body_part(self, new_part:tuple[int,int]):
+        self.coordinates.append(new_part)
+        self.body_parts += 1
         
 class Apple:
     def __init__(self):
-        self.position = (0,0) # TODO randomise
+        self.coordinates = self.__get_rdm_coord()
+        x, y = self.coordinates
+        self.color ='#FF0000'
+        canva.create_oval(x*CELL_SIZE, y*CELL_SIZE, x*CELL_SIZE+CELL_SIZE, y*CELL_SIZE+CELL_SIZE, fill=self.color)
+
+    @staticmethod
+    def __get_rdm_coord():
+        while True:
+            x = rdm.randint(0, COLUMNS-1)
+            y = rdm.randint(0, ROWS-1)
+            if (x, y) not in snake.coordinates:
+                return (x, y)
 
 # var :
 score = 0
-snake = Snake()
-apple = Apple()
 
 # config interface :
 window = tk.Tk()
@@ -47,6 +61,9 @@ for i in range(ROWS):
         else:
             color = BACKGROUND_COLOR_2
         canva.create_rectangle(x0, y0, x1, y1, fill=color)
+
+snake = Snake()
+apple = Apple()
 
 # config game :
 def init_game():
